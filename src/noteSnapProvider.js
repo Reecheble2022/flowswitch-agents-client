2f5e275c733d6ef6f49d256b0f37e8d10a21955c
Rodgers <rodgers@mahanitech.com>
2025-10-03 22:40:28 +0300

    Implement camera switch

--- src/noteSnapProvider.js
+++ src/noteSnapProvider.js
@@ -278,7 +278,7 @@ export const NoteSnapProvider = ({ children, user }) => {
                       className="w-full max-h-48 object-cover rounded-xl"
                       videoConstraints={{ facingMode }} // NEW: Dynamic facingMode
                     />
-                    <div className="flex gap-2 mt-4">
+                    <div className="flex flex-col 3xl:flex-row gap-2 mt-4">
                       <button
                         onClick={captureNote}
                         className="bg-green-600 text-white px-4 py-2 rounded hover:bg-green-700"
