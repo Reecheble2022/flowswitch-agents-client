e558affd1881e655f41a2bb3b529394f9dd14720
Rodgers <rodgers@mahanitech.com>
2025-10-04 10:33:55 +0300

    Fix agent signup

--- src/userLocationProvider.js
+++ src/userLocationProvider.js
@@ -83,7 +83,7 @@ export const UserLocationProvider = ({ children, user }) => {
   console.log("ooooooo-verficationSchedulesCount =", verficationSchedulesCount)
   console.log("ooooooo-user?.agentGuid?.verificationSchedules =", user?.agentGuid?.verificationSchedules)
   console.log("ooooooo-verficationSchedulesCount =", verficationSchedulesCount)
-  console.log("ooooooo-isDateTodayOrEarlier(2025-10-04T00:00:00.000Z) =", isDateTodayOrEarlier("2025-10-04T00:00:00.000Z"))
+  console.log("ooooooo-isDateTodayOrEarlier(2025-10-04 00:00:00.000) =", isDateTodayOrEarlier("2025-10-04T00:00:00.000Z"))
   useEffect(() => {
     if(verficationSchedulesCount && (previousVerficationSchedulesCount !== verficationSchedulesCount)){
       if ((user?.agentGuid?._id || user?.agentGuid?.guid) && (previousAgentGuid !== (user?.agentGuid?._id || user?.agentGuid?.guid))) {
